7e7e4cb9835fd5c1bcf21dbf743bd242c8cb734d
Nucci-karim <nuccikarim@gmail.com>
2023-05-19 14:43:23 +0200

    stampa nel table con bootstrap

--- index.php
+++ index.php
@@ -39,5 +39,46 @@ $hotels = [
 
     ];
 
+
     var_dump($hotels);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>php-hotel</title>
+</head>
+<body>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>nome</th>
+                <th>descizione</th>
+                <th>parcheggio</th>
+                <th>voto</th>
+                <th>distanza</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach($hotels as $elem){
+            echo 
+            '<tr>'. 
+             '<td>' . $elem['name'] . '</td>'.
+             '<td>' . $elem['description'] . '</td>'.
+             '<td>' . $elem['parking'] . '</td>'.
+             '<td>' . $elem['vote'] . '</td>'.
+             '<td>' . $elem['distance_to_center'] . 'km' . '</td>'.
+             '</tr>';
+        }
+        ?>
+        </tbody>
+        
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
